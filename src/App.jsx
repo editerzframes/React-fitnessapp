8c794ea8e71641daa9ffaaade7666f1f8852b838
Puneet G <puneet.g@codebrewinnovations.com>
2024-07-12 18:51:00 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,25 @@ import Nav from "./components/Nav";
 import Search from "./components/Search";
 import { useState } from "react";
 import "./App.css";
+import Container from "./components/Container";
+import InnerContainer from "./components/InnerContainer";
+import FoodDetails from "./components/FoodDetails";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
+  const [foodId, setFoodId] = useState("");
   return (
     <div className="App">
       <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
-      <FoodList foodData={foodData} />
+      <Container>
+        <InnerContainer>
+          <FoodList foodData={foodData} setFoodId={setFoodData} />
+        </InnerContainer>
+        <InnerContainer>
+          <FoodDetails />
+        </InnerContainer>
+      </Container>
     </div>
   );
 }
--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -1,11 +1,21 @@
-import styles from './fooditem.module.css';
+import styles from "./fooditem.module.css";
 
-export default function FoodItem({ food }) {
+export default function FoodItem({ food, setFoodId }) {
   return (
     <div className={styles.itemContainer}>
-      <img src={food.image} alt="" />
-      <h1>{food.title}</h1>
-      <button>View Recipe</button>
+      <img className={styles.itemImage} src={food.image} alt="" />
+      <div className={styles.itemContent}>
+        <p className={styles.itemName}>{food.title}</p>
+        <button
+          onClick={(e) => {
+            console.log(food.id);
+            setFoodId(food.id);
+          }}
+          className={styles.itemButton}
+        >
+          View Recipe
+        </button>
+      </div>
     </div>
   );
 }
